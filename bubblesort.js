28089663bc410fdb36f005361e6280a35efbe271
webexpert1 <richokoye1@gmail.com>
2020-08-04 20:58:55 -0700

    bubble sort implementation

---
+++ bubblesort.js
@@ -0,0 +1,20 @@
+// A sorting algorithm where the largest values bubble up to the top
+function bubbleSort(arr) {
+    let noSwaps;
+    for (let i = arr.length; i> 0; i--) {
+        noSwaps = true;
+       for (let j = 0; j < i-1; j++) {
+           if(arr[j] > arr[j+1]) {
+               let temp = arr[j];
+               arr[j] = arr[j+1];
+               arr[j+1] = temp;
+               noSwaps = false;
+           }
+           
+       }
+        if(noSwaps) break;        
+    }
+    return arr;
+}
+
+console.log(bubbleSort([5,1,2,3,4]));
